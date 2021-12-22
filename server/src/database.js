c933dc5037c3163abb257f6bb3e69320d53f1eb8
Boglárka Sebestyén <sebestyenboglarkacsilla@gmail.com>
2021-12-22 11:59:17 +0200

    Initial commit.

---
+++ server/src/database.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const User = require('./models/user.model');
+
+// const connection = 'mongodb://localhost:27017/dockerize-me';
+const connection = 'mongodb://mongo:27017/dockerize-me'; 
+
+const connectDb = () => {
+  return mongoose.connect(connection, { useNewUrlParser: true, useUnifiedTopology: true });
+};
+
+module.exports = connectDb;
+
+
+
